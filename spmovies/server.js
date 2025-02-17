2c314650e80204432552fa8a43a2541186152dab
Faye Chan <98392250+FayzeC@users.noreply.github.com>
2025-02-17 15:35:48 +0800

    Create server.js

---
+++ spmovies/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config({ path: ".env.development" });
+const app = require("./app");
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
